28830c3117a31346b78a4665f9eb8518ddf2751f
MarkovaPolina-arch <polina-markova-2001@list.ru>
2026-01-20 22:02:44 +0300

    Update task1.py

--- Lab_4/task1.py
+++ Lab_4/task1.py
@@ -1,42 +1,8 @@
 import json
 # TODO решите задачу
 def task() -> float:
-    json_content = """[
-  {
-    "score": 0.0009456152645028281,
-    "weight": 1
-  },
-  {
-    "score": 0.00020640167757499364,
-    "weight": 1
-  },
-  {
-    "score": 0,
-    "weight": 1
-  },
-  {
-    "score": 1.6557065217391307,
-    "weight": 1
-  },
-  {
-    "score": 0,
-    "weight": 1
-  },
-  {
-    "score": 0.6066065217391303,
-    "weight": 1
-  },
-  {
-    "score": 0.03126181644071977,
-    "weight": 1
-  },
-  {
-    "score": 0.001253973281817707,
-    "weight": 1
-  }
-]"""
-    data = json.loads(json_content)
+    with open('input.json', 'r') as file:
+        data = json.load(file)
     total = sum(item["score"] * item["weight"] for item in data)
     return round(total, 3)
-
 print(task())
